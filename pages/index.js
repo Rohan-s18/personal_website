a7cc30e3ccf7ec025bbd9e2df75512d2a677d174
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-03 15:44:09 -0800

    Restored to old version

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import RepositoryList from '../Components/RepositoryList'
-import Homepage from '../Components/Homepage'
 
 export default function Home(){
     
@@ -10,14 +9,14 @@ export default function Home(){
         <div className={styles.container}>
 
           <Head>
-            <title>Rohan Singh</title>
+            <title>Rohan and Alessandra's Repositories</title>
           </Head>
 
           <main className={styles.main}>
 
-            <h1 className={styles.title}>Rohan Singh: Person Formerly Known as JocularGlint54</h1>
+            <h1 className={styles.title}>Check out Rohan and Alessandra's Repositories</h1>
           
-            <Homepage />
+            <RepositoryList />
         
           </main>
 
