28a4c988160af436db991552def7668535d35ec1
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-03 17:11:33 -0800

    Fixed filepath bug

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import RepositoryList from '../Components/RepositoryList'
-import HomePage from '../Components/Homepage'
+import HomePage from '../Components/HomePage'
 
 export default function Home(){
     
