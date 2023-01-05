5ef4275ca0477da0e3b93e8c14a1b1f6207180d6
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-05 13:38:46 -0800

    Added React Component for RPI Listings

---
+++ Components/RPI_Listings.js
@@ -0,0 +1,24 @@
+//  Author: Rohan Singh
+//  1/5/2023
+//  React Component for Rohan Pain Index Listings
+
+
+//  Imports
+import Link from "next/link";
+import styles from "../styles/Home.module.css"
+
+
+//  Export Function
+export default function RPI_Listings(){
+
+    return(
+
+        <div>
+            List
+        </div>
+
+    )
+
+}
+
+
--- Components/RPI_component.js
+++ Components/RPI_component.js
@@ -9,6 +9,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
+import RPI_Listings from './RPI_Listings';
 
 
 //  Export Function
@@ -17,7 +18,33 @@ export default function RPI_component(){
     return(
 
         <div className={styles.jocs_home}>
-            Hello World!
+            
+            <h2>
+                Lab Description
+            </h2>
+
+            <p>
+                Repository for the CSDS 338 (Intro to Operating Systems) Lab for Case Western Reserve University. 
+                The lab component includes modules for the C Programming Language, Linux CLI tutorials and Bash Scripts. 
+                The main objective of this lab is to make students proficient in C and Bash by the end of the semester. 
+                This lab is completely optional and you can pace the modules based on your own preference.
+                <br></br><br></br>
+                These modules (not to be confused with C modules) will have their own README files that will contain 
+                additional details about the indiviadual files within the modules and will have links to external resources 
+                that you might find useful. You can pace these modules based on your own comfort level.
+            </p>
+
+
+
+            <br></br>
+
+            <h2>RPI Rankings</h2>
+
+            <br></br>
+
+            <RPI_Listings />
+
+
         </div>
 
     )
