9561925c921adde82ea43a7cb16bc7dfab425db6
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-05 13:03:29 -0800

    Added RPI page and Component

---
+++ Components/RPI_component.js
@@ -0,0 +1,27 @@
+//  Author: Rohan Singh
+//  1/5/2023
+//  React Component for the Rohan Pain Index
+
+
+//  Imports
+//  Imports
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+
+
+//  Export Function
+export default function RPI_component(){
+
+    return(
+
+        <div className={styles.jocs_home}>
+            Hello World!
+        </div>
+
+    )
+
+}
+
+
