47d6a652eb57b4cb426a89d6b2cc7ee5022588d3
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-05 09:19:47 -0800

    Linked Page and React Component

--- Components/Lab.js
+++ Components/Lab.js
@@ -1,3 +1,28 @@
 //  Author: Rohan Singh
 //  1/5/2023
 //  React Component for CSDS 338 Lab Page
+
+
+//  Imports
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+
+
+
+
+
+//  Export Function
+export default function Lab(){
+
+    return(
+
+        <div>
+            Hello World!
+        </div>
+
+    )
+
+}
+
--- pages/CSDS338Lab.js
+++ pages/CSDS338Lab.js
@@ -1,3 +1,42 @@
 //  Author: Rohan Singh
 //  1/5/2023
 //  Web page for CSDS 338 Lab Page
+
+
+//  Imports
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+import Lab from '../Components/Lab'
+
+
+//  Export Function
+export default function CSDS338Lab(){
+
+    return(
+
+        <div className={styles.container}>
+
+          <Head>
+            <title>CSDS 338 Lab</title>
+          </Head>
+
+          <main className={styles.main}>
+
+            <Navbar />
+
+            <h1 className={styles.title}>Operating Systems Lab</h1>
+          
+            <Lab />
+
+            <Footer />
+        
+          </main>
+
+        </div>
+
+    )
+
+}
