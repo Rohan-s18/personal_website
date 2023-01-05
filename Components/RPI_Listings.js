db2434ee97f4140bf10079b7d51dfc9bc9356e96
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-05 14:39:35 -0800

    Added RPI Cards to Listing

--- Components/RPI_Listings.js
+++ Components/RPI_Listings.js
@@ -35,10 +35,30 @@ export default function RPI_Listings(){
     return(
 
         <div className={styles.grid}>
-            
-            <RPI_Card title="Lemon on Injury" physical_score="3" emotional_score="1" piss_score="2"/>
 
-            <RPI_Card title="Stubbing your pinky" physical_score="3" emotional_score="1" piss_score="4"/>
+            <RPI_Card title="Stubbing your pinky toe" physical_score="3.2" emotional_score="1" piss_score="4.6"/>
+
+            <RPI_Card title="SAGES Essay" physical_score="1" emotional_score="10" piss_score="10"/>
+
+            <RPI_Card title="Attending Ethics Lectures" physical_score="1" emotional_score="4.7" piss_score="1.2"/>
+
+            <RPI_Card title="Ravens Losing a game" physical_score="1" emotional_score="4.4" piss_score="Fire Greg Roman"/>
+
+            <RPI_Card title="Drinking Swiss Miss" physical_score="2.2" emotional_score="2.5" piss_score="5.5"/>
+
+            <RPI_Card title="Running out of Soap" physical_score="1" emotional_score="10" piss_score="4"/>
+
+            <RPI_Card title="Communists and Hippies" physical_score="1" emotional_score="2.5" piss_score="10"/>
+
+            <RPI_Card title="Lemon on an open wound" physical_score="2.9" emotional_score="1" piss_score="2.1"/>
+
+            <RPI_Card title="Socializing with strangers" physical_score="1.5" emotional_score="3.6" piss_score="3.1"/>
+
+            <RPI_Card title="Java" physical_score="1" emotional_score="4.5" piss_score="5.5"/>
+
+            <RPI_Card title="Eating Leutner Food" physical_score="6.5" emotional_score="2.3" piss_score="1"/>
+
+            <RPI_Card title="Getting your foot snapped between chairs" physical_score="5.1" emotional_score="1.4" piss_score="2.1"/>
 
         </div>
 
