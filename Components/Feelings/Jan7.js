160191ed9a8b69d3b175f8cbf7eb8b65cf830961
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-07 09:12:17 -0800

    Added Stuff

---
+++ Components/Feelings/Jan7.js
@@ -0,0 +1,29 @@
+//  Author: Rohan Singh
+//  1/7/2023
+//  React Component for my feelings on January 7, 2023
+
+
+//  Export Function
+export default function Jan7(){
+
+    return(
+
+        <>
+            
+            <h3>
+                January 7, 2023
+            </h3>
+
+            <br></br>
+
+            <p>
+
+                Hello World!
+
+            </p>
+
+        </>
+
+    )
+
+}
